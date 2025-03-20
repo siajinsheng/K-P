0ba35deea68886541ae26bf81f2ec35aa924e07f
jinsheng-sia <108455499+jinsheng-sia@users.noreply.github.com>
2025-03-20 13:48:10 +0800

    sql

---
+++ K&P Assignment/k_p.sql
@@ -0,0 +1,385 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 20, 2025 at 06:47 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `k&p`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `address_id` varchar(255) NOT NULL,
+  `cus_id` varchar(255) NOT NULL,
+  `street` decimal(10,0) NOT NULL,
+  `city` decimal(10,0) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `post_code` date NOT NULL,
+  `country` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` varchar(255) NOT NULL,
+  `admin_name` varchar(255) NOT NULL,
+  `admin_password` varchar(255) NOT NULL,
+  `con_admin_password` varchar(255) NOT NULL,
+  `admin_update_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `admin_email` varchar(255) NOT NULL,
+  `admin_status` varchar(255) NOT NULL,
+  `admin_profile_pic` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE `bank` (
+  `card_no` varchar(255) NOT NULL,
+  `payment_id` varchar(255) NOT NULL,
+  `card_name` varchar(255) NOT NULL,
+  `expired_date` date NOT NULL,
+  `cvv` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `category_name` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cus_id` varchar(255) NOT NULL,
+  `cus_name` varchar(255) DEFAULT NULL,
+  `cus_Email` varchar(255) NOT NULL,
+  `cus_password` varchar(255) NOT NULL,
+  `con_cus_password` varchar(255) NOT NULL,
+  `cus_gender` decimal(10,0) DEFAULT NULL,
+  `cus_profile_pic` varchar(255) DEFAULT NULL,
+  `cus_update_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `cus_status` varchar(255) NOT NULL,
+  `role` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `delivery`
+--
+
+CREATE TABLE `delivery` (
+  `delivery_id` varchar(255) NOT NULL,
+  `address_id` varchar(255) NOT NULL,
+  `delivery_fee` decimal(10,0) NOT NULL,
+  `delivery_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `discount`
+--
+
+CREATE TABLE `discount` (
+  `Discount_id` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL,
+  `discount_rate` decimal(10,0) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `e_wallet`
+--
+
+CREATE TABLE `e_wallet` (
+  `fng_no` varchar(255) NOT NULL,
+  `payment_id` varchar(255) NOT NULL,
+  `otp` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orders_id` varchar(255) NOT NULL,
+  `delivery_id` varchar(255) NOT NULL,
+  `cus_id` varchar(255) NOT NULL,
+  `order_date` date NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `orders_status` varchar(255) NOT NULL,
+  `prod_dis_price` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_list`
+--
+
+CREATE TABLE `order_list` (
+  `order_id` varchar(255) NOT NULL,
+  `product_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` varchar(255) NOT NULL,
+  `orders_id` varchar(255) NOT NULL,
+  `tax` decimal(10,0) NOT NULL,
+  `total_amount` decimal(10,0) NOT NULL,
+  `payment_status` varchar(255) NOT NULL,
+  `payment_date` date NOT NULL,
+  `payment_method` varchar(255) DEFAULT NULL,
+  `discount` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` varchar(255) NOT NULL,
+  `product_pic1` varchar(255) DEFAULT NULL,
+  `product_pic2` varchar(255) DEFAULT NULL,
+  `product_pic3` varchar(255) DEFAULT NULL,
+  `product_pic4` varchar(255) DEFAULT NULL,
+  `product_pic5` varchar(255) DEFAULT NULL,
+  `product_pic6` varchar(255) DEFAULT NULL,
+  `product_pic7` varchar(255) DEFAULT NULL,
+  `product_pic8` varchar(255) DEFAULT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_description` varchar(255) NOT NULL,
+  `product_price` decimal(10,0) NOT NULL,
+  `product_status` varchar(255) NOT NULL,
+  `product_stock` int(11) NOT NULL,
+  `product_sell` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voucher`
+--
+
+CREATE TABLE `voucher` (
+  `voucher_code` varchar(255) NOT NULL,
+  `cus_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `discount_rate` decimal(10,0) NOT NULL,
+  `point_need` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`address_id`),
+  ADD KEY `cus_id` (`cus_id`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`card_no`),
+  ADD KEY `payment_id` (`payment_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cus_id`);
+
+--
+-- Indexes for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD PRIMARY KEY (`delivery_id`),
+  ADD KEY `address_id` (`address_id`);
+
+--
+-- Indexes for table `discount`
+--
+ALTER TABLE `discount`
+  ADD PRIMARY KEY (`Discount_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `e_wallet`
+--
+ALTER TABLE `e_wallet`
+  ADD PRIMARY KEY (`fng_no`),
+  ADD KEY `payment_id` (`payment_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orders_id`),
+  ADD KEY `delivery_id` (`delivery_id`),
+  ADD KEY `cus_id` (`cus_id`);
+
+--
+-- Indexes for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`),
+  ADD KEY `orders_id` (`orders_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `voucher`
+--
+ALTER TABLE `voucher`
+  ADD PRIMARY KEY (`voucher_code`),
+  ADD KEY `cus_id` (`cus_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `address_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`);
+
+--
+-- Constraints for table `bank`
+--
+ALTER TABLE `bank`
+  ADD CONSTRAINT `bank_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`payment_id`);
+
+--
+-- Constraints for table `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+
+--
+-- Constraints for table `delivery`
+--
+ALTER TABLE `delivery`
+  ADD CONSTRAINT `delivery_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`);
+
+--
+-- Constraints for table `discount`
+--
+ALTER TABLE `discount`
+  ADD CONSTRAINT `discount_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+
+--
+-- Constraints for table `e_wallet`
+--
+ALTER TABLE `e_wallet`
+  ADD CONSTRAINT `e_wallet_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`payment_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`delivery_id`) REFERENCES `delivery` (`delivery_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`);
+
+--
+-- Constraints for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD CONSTRAINT `order_list_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`orders_id`);
+
+--
+-- Constraints for table `voucher`
+--
+ALTER TABLE `voucher`
+  ADD CONSTRAINT `voucher_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
